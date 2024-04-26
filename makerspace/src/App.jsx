b40af2ae3e83a28ee48d920334d5bd76eb91ac0b
Ganesh Krishna <63455112+Ganesh2914@users.noreply.github.com>
2024-04-26 13:07:43 -0500

    Added Buttons Sign In
    Added buttons on sing up and sign in

--- makerspace/src/App.jsx
+++ makerspace/src/App.jsx
@@ -3,6 +3,7 @@ import NavigationBar from './components/NavigationBar'
 import {BrowserRouter, Route, Routes } from "react-router-dom"
 import './App.css'
 import Signup from './pages/Signup';
+import Signin from './pages/Signin';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <NavigationBar />
         <Routes>
           <Route path='/signup' element={<Signup/>}></Route>
+          <Route path='/signin' element={<Signin/>}></Route>
         </Routes>
       
       </BrowserRouter>
--- makerspace/src/components/NavigationBar.jsx
+++ makerspace/src/components/NavigationBar.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import Account from './Account';
 
 const NavigationBar = () => {
+  function slideDown(){
+      return<>
+       <div id="dropdown" className="mt-44 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700" aria-labelledby="dropdownHoverButton">
+                  <ul className="py-2 text-sm text-gray-700 dark:text-gray-200"  >
+                    <li>
+                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">REGISTER</a>
+                    </li>
+                    <li>
+                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">LOG IN </a>
+                    </li>
+                  </ul>
+                </div>
+                </>
+  }
   return (
     <nav className="bg-gray-800 shadow-md">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <div className="flex-shrink-0">
-            <a  className="text-white text-lg font-semibold">Logo</a>
+            <a className="text-white text-lg font-semibold">Logo</a>
           </div>
           {/* Navigation Links */}
           <div className="hidden md:block">
@@ -16,7 +31,7 @@ const NavigationBar = () => {
               <a className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
               <a className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Services</a>
               <a className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>
-              <a className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Account</a>
+              <Account />
             </div>
           </div>
         </div>
