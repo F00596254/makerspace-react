8b29e35359a02f66fecf76133a33a08845c987cf
Sravya C <sravyachouderpally@example.com>
2024-05-15 15:49:54 -0500

    Added signin

--- makerspace_node/config/db.js
+++ makerspace_node/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect('mongodb://localhost:27017/', {
+        const conn = await mongoose.connect('mongodb://localhost:27017/makerspace', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
--- makerspace_node/controllers/userController.js
+++ makerspace_node/controllers/userController.js
@@ -11,3 +11,30 @@ exports.signup = async (req, res) => {
     res.status(500).json({ error: 'An error occurred while processing your request' });
   }
 };
+
+
+exports.signin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    
+    // Check if the user exists
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        return res.status(200).json({ authenticated: false });
+    }
+
+    // Check if the password is correct
+    const isPasswordValid = await user.comparePassword(password);
+
+    if (!isPasswordValid) {
+        return res.status(200).json({ authenticated: false });
+    }
+
+    // If the email and password are valid, return true
+    res.status(200).json({ authenticated: true });
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'An error occurred while processing your request' });
+  }
+};
