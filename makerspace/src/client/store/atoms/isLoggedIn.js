d9d0bd826873ce89a389705b1e83c1dab213ee0e
Sravya Chouderpally <sravya.chouderpally@cuw.edu>
2024-06-30 19:44:01 -0500

    cleared conflicts

--- makerspace/src/client/store/atoms/isLoggedIn.js
+++ makerspace/src/client/store/atoms/isLoggedIn.js
@@ -3,7 +3,8 @@ let token = localStorage.getItem("token");
 if (token == "" || token == undefined) {
   token = false;
 }
-const defaultLoggedIn = token !== null ? JSON.parse(token) : false;
+// const defaultLoggedIn = token !== null ? JSON.parse(token) : false;
+const defaultLoggedIn = token !== null ? token : false;
 export const isLoggedIn=atom({
     key:"isLoggedIn",
     default:defaultLoggedIn
